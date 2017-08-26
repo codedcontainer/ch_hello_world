757e539a2e960adc361a78ea68ec595eb8ff20d7
jutt <jutt@iupui.edu>
2017-08-26 12:44:43 -0400

    Form validation

--- ch_hello_world/Models/Name.cs
+++ ch_hello_world/Models/Name.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations; 
 
 namespace ch_hello_world.Models
 {
-    public class Name
+    public class Name 
     {
         public int ID { get; set; }
+        [StringLength(255, MinimumLength = 2)]
+        [Display(Name="First Name")]
+        [Required]
+        [RegularExpression(@"^([A-Z][a-z]*)")]
         public string firstName { get; set; }
+        [StringLength(255, MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        [Required]
+        [RegularExpression(@"^([A-Z][a-z]*)")]
         public string lastName { get; set;  }
     }
 }
